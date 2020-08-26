8c56c217e53fdcf6d24fc37fbb4aac027098e56a
Kbaukas <karolisbaukas5@gmai.com>
2020-08-26 17:06:05 +0300

    First commit
    Start of project.

---
+++ que/src/test/java/lt/kb/que/QueApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.kb.que;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
