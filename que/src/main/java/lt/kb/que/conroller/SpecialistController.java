df4ca6ab0da5a8524574b93c6c73923052d936be
Kbaukas <karolisbaukas5@gmai.com>
2020-08-31 19:54:48 +0300

    small update

--- que/src/main/java/lt/kb/que/conroller/SpecialistController.java
+++ que/src/main/java/lt/kb/que/conroller/SpecialistController.java
@@ -4,6 +4,7 @@ import lt.kb.que.model.Specialist;
 import lt.kb.que.model.Ticket;
 import lt.kb.que.service.SpecialistService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -19,30 +20,35 @@ public class SpecialistController {
     SpecialistService specialistService;
 
     @GetMapping("/specialists")
-    String showAll(Model model){
-        List<Specialist> specialists=specialistService.findAll();
-        model.addAttribute("specialists",specialists);
+    String showAll(Model model) {
+        List<Specialist> specialists = specialistService.findAll();
+        model.addAttribute("specialists", specialists);
         return "service/specialists";
 
     }
+
     @GetMapping("/specialists/{id}")
-    public String getSpecialistById(@PathVariable int id,Model model) {
-       Optional<Specialist> specialist=specialistService.findById(id);
-       if(!specialist.isPresent()){
-           return "error/index";
-       }
+    public String getSpecialistById(@PathVariable int id, Model model) {
+        System.out.println("-----------------------------------------------------");
+        System.out.println(SecurityContextHolder.getContext().getAuthentication());
+        Optional<Specialist> specialist = specialistService.findById(id);
+        if (!specialist.isPresent()) {
+            return "error/index";
+        }
         specialist.ifPresent(value -> model.addAttribute("specialist", value));
-       return "service/specialist";
+        return "service/specialist";
     }
 
 
     @GetMapping("/specialists/create")
     public String createSpecialistForm(Model model) {
-        Specialist specialist=new Specialist();
+
+        Specialist specialist = new Specialist();
         model.addAttribute("specialist", specialist);
         return "service/createSpecialist";
     }
-    @RequestMapping(value = "/specialists/create", method= RequestMethod.POST)
+
+    @RequestMapping(value = "/specialists/create", method = RequestMethod.POST)
     public String addTicket(@ModelAttribute("specialist") Specialist specialist, BindingResult result, Model model) {
         if (result.hasErrors()) {
             System.out.println("klaida");
